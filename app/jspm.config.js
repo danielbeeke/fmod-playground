3ab8332ed8da614cd80fbf5a043bfc6163317090
Daniel Beeke <daniel@studiofonkel.nl>
2017-07-11 20:13:08 +0200

    Initial commit

---
+++ app/jspm.config.js
@@ -0,0 +1,57 @@
+SystemJS.config({
+  paths: {
+    "github:": "lib/github/",
+    "npm:": "lib/npm/",
+    "fmodPlayground/": "js/fmodPlayground/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.21",
+      "fs": "npm:jspm-nodelibs-fs@0.2.1",
+      "path": "npm:jspm-nodelibs-path@0.2.3"
+    },
+    "packages": {
+      "npm:babel-runtime@5.8.38": {
+        "map": {}
+      },
+      "npm:core-js@1.2.7": {
+        "map": {
+          "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  map: {
+    "babel": "npm:babel-core@5.8.38",
+  },
+  packages: {
+    "fmodPlayground": {
+      "main": "app.js"
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {
+    "assert": "npm:jspm-nodelibs-assert@0.2.1",
+    "process": "npm:jspm-nodelibs-process@0.2.1"
+  },
+  packages: {}
+});
